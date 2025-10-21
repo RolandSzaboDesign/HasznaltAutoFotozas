25f0839df8b4da67b1212bfb73a62cb5ac0be030
Roland Szabó <hi@rolandszabo.design>
2025-10-21 11:51:25 +0200

    v1.1
    Redesigned color scheme
    Optimized texts all across the site
    Added a new About section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -129,42 +129,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-// Hero Typing Animation
-document.addEventListener("DOMContentLoaded", function () {
-	const el = document.getElementById("js-benefits");
-	const benefits = ["gyorsabban", "drágábban", "idegeskedés nélkül", "profi képekkel"];
-	let benefitIndex = 0;
-	let charIndex = 0;
-	let typing = true;
-
-	function type() {
-		if (typing) {
-			if (charIndex < benefits[benefitIndex].length) {
-				el.textContent += benefits[benefitIndex].charAt(charIndex);
-				charIndex++;
-				setTimeout(type, 60);
-			} else {
-				typing = false;
-				setTimeout(type, 1000);
-			}
-		} else {
-			if (charIndex > 0) {
-				el.textContent = benefits[benefitIndex].substring(0, charIndex - 1);
-				charIndex--;
-				setTimeout(type, 15);
-			} else {
-				typing = true;
-				benefitIndex = (benefitIndex + 1) % benefits.length;
-				setTimeout(type, 250);
-			}
-		}
-	}
-
-	type();
-});
-
-
-
 // Lightbox
 const lightbox = document.getElementById('lightbox');
 const prevBtn = lightbox.querySelector('.lightbox__arrow--prev');
